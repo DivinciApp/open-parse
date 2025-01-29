2d075b543e764dc736edcc214e742486fddc1f40
Michael Mooring <mike@divinci.app>
2025-01-29 13:46:21 -0800

    AS-171 readthedocs, 2

---
+++ docs/config.py
@@ -0,0 +1,28 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'OpenParse'
+copyright = '2024'
+author = 'Michael Mooring'
+release = '0.7.2'
+
+extensions = [
+    'sphinx.ext.autodoc',    # API documentation 
+    'sphinx.ext.napoleon',   # Support for Google/NumPy docstrings
+    'sphinx.ext.viewcode',   # Add links to source code
+    'myst_parser',          # Support markdown files
+]
+
+# Source file parsers
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# Theme settings
+html_theme = 'sphinx_rtd_theme'
+
+# Output options
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
